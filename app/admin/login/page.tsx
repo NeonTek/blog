204993216a95841a3280c8d564bc5a119454e5ff
Wesley Weiss <neontek6@gmail.com>
2025-07-16 18:48:27 +0300

    Forcing password te be red as string

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -24,7 +24,7 @@ export default function AdminLoginPage() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
-    if (password !== process.env.ADMIN_PASSWORD) {
+    if (password !== process.env.ADMIN_PASSWORD?.toString()) {
       toast({
         title: "Access Denied",
         description: "Invalid password",
